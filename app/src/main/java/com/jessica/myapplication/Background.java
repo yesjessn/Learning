a83be1bbfe27b1bd11a20af48c1e8388a41c69ad
yesjessn <yelnguyen@ucdavis.edu>
2016-07-14 14:51:33 -0700

    3

---
+++ app/src/main/java/com/jessica/myapplication/Background.java
@@ -0,0 +1,41 @@
+package com.jessica.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+/**
+ * Created by jessica on 7/14/2016.
+ */
+public class Background {
+
+    private Bitmap image;
+    private int x, y, dx;
+
+    public Background(Bitmap res)
+    {
+        image = res;
+    }
+    public void update()
+    {
+        //background moves across screen
+        x += dx;
+        //resets background to original spot
+        if(x<-GamePanel.WIDTH){
+            x = 0;
+        }
+    }
+    public void draw(Canvas canvas)
+    {
+        canvas.drawBitmap(image, x, y, null);
+        //when x is not in starting position...
+        if(x<0)
+        {
+            //...puts new background right after the old one
+            canvas.drawBitmap(image, x + GamePanel.WIDTH, y, null);
+        }
+    }
+    public void setVector(int dx)
+    {
+        this.dx = dx;
+    }
+}
--- app/src/main/java/com/jessica/myapplication/GamePanel.java
+++ app/src/main/java/com/jessica/myapplication/GamePanel.java
@@ -1,6 +1,8 @@
 package com.jessica.myapplication;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -9,11 +11,14 @@ import android.view.SurfaceView;
  * Created by jessica on 7/14/2016.
  */
 
-public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
-{
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
+    //background image size
+    public static final int WIDTH = 856;
+    public static final int HEIGHT = 480;
     private MainThread thread;
-    public GamePanel(Context context)
-    {
+    private Background bg;
+
+    public GamePanel(Context context) {
         super(context);
 
 
@@ -27,36 +32,59 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
     }
 
     @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){}
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
 
     @Override
-    public void surfaceDestroyed(SurfaceHolder holder){
+    public void surfaceDestroyed(SurfaceHolder holder) {
         boolean retry = true;
-        while(retry)
-        {
-            try{thread.setRunning(false);
+        while (retry) {
+            try {
+                thread.setRunning(false);
                 thread.join();
 
-            }catch(InterruptedException e){e.printStackTrace();}
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             retry = false;
         }
     }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder){
+    public void surfaceCreated(SurfaceHolder holder) {
 
+        bg = new Background(BitmapFactory.decodeResource(getResources(), R.drawable.grassbg1));
+        bg.setVector(-5);
         //we can safely start the game loop
         thread.setRunning(true);
         thread.start();
 
     }
+
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
+    public boolean onTouchEvent(MotionEvent event) {
         return super.onTouchEvent(event);
     }
-    public void update()
-    {
 
+    public void update() {
+        bg.update();
+    }
+
+    @Override
+    public void draw(Canvas canvas)
+    {
+        //gets width of the entire surface screen
+        final float scaleFactorX = getWidth()/WIDTH;
+        //gets height of entire surface screen
+        final float scaleFactorY = getHeight()/HEIGHT;
+        if (canvas != null) {
+            final int savedState = canvas.save();
+            //scales screen
+            canvas.scale(scaleFactorX, scaleFactorY);
+            //draws screen
+            bg.draw(canvas);
+            //stops infinite loop of scaling
+            canvas.restoreToCount(savedState);
+        }
     }
 }
